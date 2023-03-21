a7708512a8a0a5e1ca6ba2860b04770b870cabe1
fizdalf <fizdalf@gmail.com>
2023-03-21 19:15:58 +0000

    [refactor]: rename and reorder tests to be more expressive of the domain

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,31 +1,21 @@
 import {fizzbuzz} from './fizzbuzz';
 
 describe("fizzbuzz", () => {
-    it("should return '1' when given 1", () => {
+    it("should return the given number as string", () => {
         expect(fizzbuzz(1)).toBe("1");
-    });
-    it("should return '2' when given 2", () => {
         expect(fizzbuzz(2)).toBe("2");
-    });
-    it("should return 'Fizz' when given 3", () => {
-        expect(fizzbuzz(3)).toBe("Fizz");
-    });
-    it("should return '4' when given 4", () => {
         expect(fizzbuzz(4)).toBe("4");
     });
-    it("should return 'Buzz' when given 5", () => {
-        expect(fizzbuzz(5)).toBe("Buzz");
-    });
-    it("should return 'Fizz' when given 6", () => {
+    it("should return 'Fizz' when given multiples of 3", () => {
+        expect(fizzbuzz(3)).toBe("Fizz");
         expect(fizzbuzz(6)).toBe("Fizz");
-    });
-    it("should return 'Fizz' when given 9", () => {
         expect(fizzbuzz(9)).toBe("Fizz");
     });
-    it("should return 'Buzz' when given 10", () => {
+    it("should return 'Buzz' when given multiples of 5", () => {
+        expect(fizzbuzz(5)).toBe("Buzz");
         expect(fizzbuzz(10)).toBe("Buzz");
     });
-    it("should return 'FizzBuzz' when given 15", () => {
+    it("should return 'FizzBuzz' when given multiples of both 3 and 5", () => {
         expect(fizzbuzz(15)).toBe("FizzBuzz");
     });
     it("should error when given numbers smaller than 1", () => {
@@ -34,5 +24,4 @@ describe("fizzbuzz", () => {
     it("should error when given numbers greater than 100", () => {
         expect(() => fizzbuzz(101)).toThrow("Provide a number between 1 and 100");
     });
-
 });
