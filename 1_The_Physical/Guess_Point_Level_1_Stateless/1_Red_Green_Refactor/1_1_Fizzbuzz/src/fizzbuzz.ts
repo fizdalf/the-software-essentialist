8f5bb5169f5729fbe269478b858b5d871b4d803a
fizdalf <fizdalf@gmail.com>
2023-03-21 16:29:56 +0000

    [new]: fizzbuzz returns "2" when given 2

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,6 @@
 export function fizzbuzz(givenNumber: number): string {
+    if (givenNumber === 2) {
+        return "2";
+    }
     return "1";
 }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,5 +4,7 @@ describe("fizzbuzz", () => {
     it("should return '1' when given 1", () => {
         expect(fizzbuzz(1)).toBe("1");
     });
-
+    it("should return '2' when given 2", () => {
+        expect(fizzbuzz(2)).toBe("2");
+    });
 });
