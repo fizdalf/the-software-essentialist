e4ca621ca5c7d4890f7bf6403a17129d87dabab0
fizdalf <fizdalf@gmail.com>
2023-03-21 16:33:25 +0000

    [refactor]: fizzbuzz returns the given number transformed into string if not 3

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,12 +1,12 @@
 export function fizzbuzz(givenNumber: number): string {
     if (givenNumber === 4) {
-        return "4";
+        return `${givenNumber}`;
     }
     if (givenNumber === 3) {
         return "Fizz";
     }
     if (givenNumber === 2) {
-        return "2";
+        return `${givenNumber}`;
     }
-    return "1";
+    return `${givenNumber}`;
 }
