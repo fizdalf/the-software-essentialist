cbb6196af1baccbc28e2bfb5f2f87511b0da4107
fizdalf <fizdalf@gmail.com>
2023-03-21 16:34:33 +0000

    [refactor]: Rule of Three, remove repetition.

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,12 +1,7 @@
 export function fizzbuzz(givenNumber: number): string {
-    if (givenNumber === 4) {
-        return `${givenNumber}`;
-    }
     if (givenNumber === 3) {
         return "Fizz";
     }
-    if (givenNumber === 2) {
-        return `${givenNumber}`;
-    }
+
     return `${givenNumber}`;
 }
